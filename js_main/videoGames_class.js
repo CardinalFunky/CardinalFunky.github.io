4dddcf83835f514d6d0702fef4f80568e28630c4
Cardinal Funky <Dequan.medina@gmail.com>
2019-10-02 15:40:31 -0400

    Updating how the objects are defined and used

--- js_main/videoGames_class.js
+++ js_main/videoGames_class.js
@@ -1,429 +1,53 @@
 /* **************************************************************************************
-    This file defines an array of video game objects for the website.
+    This file defines objects of video games for the website.
 ***************************************************************************************** */
  
-const videoGames = [
+const games = [
     {
+        id: 1,
         name: "Monster Hunter Generations Ultimate",
-        systems: ["Nintendo Switch"],
-        generes: ["Action", "Role-Playing"]
     },
     {
+        id: 2,
         name: "Tetris 99",
-        systems: ["Nintendo Switch"],
-        genres: ["Multiplayer", "Puzzle"]
     },
     {
+        id: 3,
         name: "Super Smash Bros. Ultimate",
-        systems: ["Nintendo Switch"],
-        genres: ["Action", "Fighting", "Multiplayer"]
     },
     {
+        id: 4,
         name: "Pokemon: Let's Go, Pikachu!",
-        systems: ["Nintendo Switch"],
-        genres: ["Adventure", "RPG", "Multiplayer"]
     },
     {
+        id: 5,
         name: "Path of Exile",
-        systems: ["PC", "Steam"],
-        genres: ["Action", "Adventure", "Free to Play", "Indie", "Massively Multiplayer", "RPG"]
     },
     {
+        id: 6,
         name: "Attack on Titan / A.O.T. Wings of Freedom",
-        systems: ["PC", "Steam"],
-        genres: ["Action"]
-    },
-    {
-        name: "Shadow Warrior 2",
-        systems: ["PC", "Steam"],
-        genres: ["Action", "Adventure"]
-    },
-    {
-        name: "Undertale",
-        systems: ["PC", "Steam"],
-        genres: ["Indie", "RPG"]
-    },
-    {
-        name: "Vikings - Wolves of Midgard",
-        systems: ["PC", "Steam"],
-        genres: ["Action", "RPG"]
-    },
-    {
-        name: "No Man's Sky",
-        systems: ["PC", "GOG Galaxy"],
-        genres: ["Adventure", "Action", "Sandbox"]
-    },
-    {
-        name: "Neverwinter Nights Diamond",
-        systems: ["PC", "GOG Galaxy"],
-        genres: ["RPG"]
-    },
-    {
-        names: "Ultima™ 4: Quest of the Avatar",
-        systems: ["PC", "GOG Galaxy"],
-        genres: ["RPG", "Adventure", "Fantasy"]
-    },
-    {
-        name: "Battlefield Bad Company 2",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Battlefield 3",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Battlefield 4",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Mass Effect: Andromeda",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Titanfall 2",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Bejeweled 3",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Command & Conquer Red Alert 2 and Yuri's Revenge",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Crusader: No Remorse",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Crysis 2 Maximum Edition",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Dead Space",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Dead Space 3",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Dragon Age II",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Dragon Age Inquisition",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Dragon Age Origins",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Dragon Age: Origins - Ultimate Edition",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Dungeon Keeper",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Jade Empire Special Edition",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Mass Effect",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Mass Effect 2",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Mass Effect 3",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Medal of Honor Pacific Assault",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Nox",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Peggle",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Plants vs. Zombies",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Populous",
-        genres: ["PC", "Origin"],
-        systems: []
-    },
-    {
-        name: "Sim City 2000: The Ultimate City Simulator",
-        genres: ["PC", "Origin"],
-        systems: []
-    },
-    {
-        name: "Syndicate (1993)",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "The Sims 3",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Theme Hospital",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Titanfall",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Ultima VIII Gold Edition",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Wing Commander III: Heart of the Tiger",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Zuma's Revenge",
-        systems: ["PC", "Origin"],
-        genres: []
-    },
-    {
-        name: "Subnautica",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "ABZU",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "Fortnite",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "City of Brass",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "Everything",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "Hyper Light Drifter",
-        systems: ["PC", "Epic Games", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Last Day of June",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "Metro: 2033 Redux",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "Mutant Year Zero Road to Eden",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "PixArk",
-        systems: ["PC", "Epic Games", "Steam"],
-        genres: []
-    },
-    {
-        name: "RiME",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "Stories Untold",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "The End is Nigh",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "Transistor",
-        systems: ["PC", "Epic Games"],
-        genres: []
-    },
-    {
-        name: "A Good Snowman Is Hard To Build",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Bomber Crew",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Broforce",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Cosmic Express",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Crossing Souls",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Dear, Esther",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Downwell",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Hotline Miami",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Hotline Miami 2: Wrong Number",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Orwell",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Orwell: Ignorance Is Strength",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Pikuniku",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Republique",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Sokobond",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Strafe",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "The Flame In The Flood",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "The Messenger",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "The Swords of Ditto",
-        systems: ["PC", "Twitch"],
-        genres: []
-    },
-    {
-        name: "Overwatch",
-        systems: ["PC", "Blizzard"],
-        genres: []
-    },
-    {
-        name: "Diablo III + Reaper of Souls",
-        systems: ["PC", "Blizzard"],
-        genres: []
-    },
-    {
-        name: "World of Warcraft: Battle for Azeroth",
-        systems: ["PC", "Blizzard"],
-        genres: []
     }
+]
+
+const systems = [
+    {
+        id: 1,
+        name: "Nintendo Switch",
+        gamesID: [1, 2, 3, 4]
+    },
+    {
+        id: 2,
+        name: "PC",
+        gamesID: [5, 6]
+    },
 ]
 
 // A chart that displays the amount of video games for each category of
 // different video game systems.
-const collectionChart = function(games) {
-    // Video Game console variables
-    var nintendoSwitch = 0;
-    var pc = 0;
-
+const collectionChart = function(systems) {
     // Get data for chart, for each video game system
-    for(var i = 0; i < games.length; i++) {
-        var gameSystems = games[i].systems;
-        
-        for(var k = 0; k < gameSystems.length; k++) {
-            if(gameSystems[k] == "Nintendo Switch") {
-                nintendoSwitch += 1;
-            } else if (gameSystems[k] == "PC") {
-                pc += 1;
-            }
-        }
-    }
+    var nintendoSwitch = systems.find(x => x.id === 1).gamesID.length;
+    var pc = systems.find(x => x.id === 2).gamesID.length;
 
     // Make and append Chart
     var canvas = "<canvas id='collectionChart' width='400' height='200'></canvas>";
