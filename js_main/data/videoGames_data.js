0ecc4d32b2cd484e1b86cd0d46f9eda120f59f00
Cardinal Funky <Dequan.medina@gmail.com>
2020-04-17 01:11:51 -0400

    Created Video Game js data file

---
+++ js_main/data/videoGames_data.js
@@ -0,0 +1,24 @@
+/**
+ * This file contains hard-coded data of video games and their information.
+ * Each video game will have an: 
+ * "id", "name", "picture", "own", "wishlist", "completed"
+ * 
+ * "id" => The key
+ * "name" => The name of the video game
+ * "picture" => The picture of the video game
+ * "owned" => A boolean that tells me if I own the video game
+ * "wishlist" => A boolean that tells me if this game is on my wishlist
+ * "completed" => A boolean that tells me if I have completed this video game
+ */
+
+const videoGames = [
+    {
+        "id": "1",
+        "name": "Attack on Titan / A.O.T. Wings of Freedom",
+        "picture": "https://steamcdn-a.akamaihd.net/steam/apps/449800/header.jpg?t=1550182918",
+        "owned": true,
+        "wishlist": false,
+        "completed": true
+    },
+
+];
