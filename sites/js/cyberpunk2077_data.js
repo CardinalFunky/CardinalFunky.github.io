a59226d39ffd20e2b387c57dbb3d1e111d3858a4
Cardinal Funky <cardinalfunky@gmail.com>
2021-01-02 02:20:30 -0500

    Created a site for Cyberpunk 2077

---
+++ sites/js/cyberpunk2077_data.js
@@ -0,0 +1,7 @@
+const tarot_cards = {
+    "1": {
+        "name":"The Fool",
+        "description":"The Fool is everyone - including you and me. Each step he takes on his journey feels like stepping into a brave new world. Ultimately, the journey will change him. But as the card shows, he's a trustworthy lad whose tireless hope drives him toward his goal.",
+        "image":"../images/cyberpunk2077/thefool.png"
+    }
+}
