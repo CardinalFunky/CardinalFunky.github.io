7a0c9a5f18094a3c7cfdaa34bbe05df79b191c68
Cardinal Funky <cardinalfunky@gmail.com>
2021-01-03 02:50:57 -0500

    updated Cyberpunk 2077 site

--- sites/js/cyberpunk2077_data.js
+++ sites/js/cyberpunk2077_data.js
@@ -3,5 +3,15 @@ const tarot_cards = {
         "name":"The Fool",
         "description":"The Fool is everyone - including you and me. Each step he takes on his journey feels like stepping into a brave new world. Ultimately, the journey will change him. But as the card shows, he's a trustworthy lad whose tireless hope drives him toward his goal.",
         "image":"images/cyberpunk2077/thefool.png"
+    },
+    "2": {
+        "name": "The Magician",
+        "description": "The Magician is the card of self-confidence and adapting to situations through intellect and sheer will. The Magician is a schemer who always has one last trick up his sleeve, who despite everything manages to stay afloat and remain in control of his own destiny.",
+        "image": "images/cyberpunk2077/theMagician.png"
+    },
+    "3": {
+        "name": "The High Priestess",
+        "description": "The High Priestess is a card of mystery. It shows how all our secrets hang by a delicate thread and the struggle between common sense and intuition. The High Priestess symbolizes the cold, calm waters as well as the mysteries hidden in their depths.",
+        "image": "images/cyberpunk2077/theHighPriestess.png"
     }
 }
