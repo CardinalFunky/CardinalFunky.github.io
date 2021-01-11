6d775c01ea27931eb31328d8b640b849a50ffc46
Cardinal Funky <cardinalfunky@gmail.com>
2021-01-11 01:06:34 -0500

    updated cyberpunk 2077

--- sites/js/cyberpunk2077_data.js
+++ sites/js/cyberpunk2077_data.js
@@ -26,58 +26,72 @@ const tarot_cards = {
     },
     "6": {
         "name": "The Hierophant",
-        "description": "The Hierophant symbolizes a respect for tradition. It represents one who tries to maintain the established order, even though their very character is shaped by it. The Hierophant places his faith in institutions - for the alternative is pure chaos. Only by placing his faith in order can he draw strength."
+        "description": "The Hierophant symbolizes a respect for tradition. It represents one who tries to maintain the established order, even though their very character is shaped by it. The Hierophant places his faith in institutions - for the alternative is pure chaos. Only by placing his faith in order can he draw strength.",
+        "image": "images/cyberpunk2077/theHierophant.png"
     },
     "7": {
         "name": "The Lovers",
-        "description": "The Lovers is the card of dichotomies. It points to the contradictions that clash within each of us and of the challenge of striking a balance between extremes. The Lovers is also the card of dilemmas, like The Fool who stands at the crossroads, unable to make his choice."
+        "description": "The Lovers is the card of dichotomies. It points to the contradictions that clash within each of us and of the challenge of striking a balance between extremes. The Lovers is also the card of dilemmas, like The Fool who stands at the crossroads, unable to make his choice.",
+        "image": "images/cyberpunk2077/theLovers.png"
     },
     "8": {
         "name": "The Chariot",
-        "description": "The Chariot is always charging ahead despite being pulled by its steeds in opposite directions. The rider who steers it constantly reins in the light and dark sides of the soul with the help of Reason. To ride in The Chariot is to experience highs and lows - ups and downs."
+        "description": "The Chariot is always charging ahead despite being pulled by its steeds in opposite directions. The rider who steers it constantly reins in the light and dark sides of the soul with the help of Reason. To ride in The Chariot is to experience highs and lows - ups and downs.",
+        "image": "images/cyberpunk2077/theChariot.png"
     },
     "9": {
         "name": "Strength",
-        "description": "Strength is the card of resilience. It is associated with determination, bravery and internal struggle. One must have dedication in order to overcome obstacles and reach one's goal. Strength is about physical prowess and spiritual fortitude - the power that must be unleashed to achieve the impossible."
+        "description": "Strength is the card of resilience. It is associated with determination, bravery and internal struggle. One must have dedication in order to overcome obstacles and reach one's goal. Strength is about physical prowess and spiritual fortitude - the power that must be unleashed to achieve the impossible.",
+        "image": "images/cyberpunk2077/strength.png"
     },
     "10": {
         "name": "The Hermit",
-        "description": "The Hermit is a card of self-imposed isolation. It represents an escape from the hustle and bustle of the city, a turning away from constant newness toward Ye Olde ways. For the hermit, solitude is the road to the sublime - a road that is raken not with great bounds and strides, but with small, concentrated steps."
+        "description": "The Hermit is a card of self-imposed isolation. It represents an escape from the hustle and bustle of the city, a turning away from constant newness toward Ye Olde ways. For the hermit, solitude is the road to the sublime - a road that is raken not with great bounds and strides, but with small, concentrated steps.",
+        "image": "images/cyberpunk2077/theHermit.png"
     },
     "11": {
         "name": "Wheel of Fortune",
-        "description": "The Wheel of Fortune means that change is coming. One's destiny could turn out for the better or the worse, yet it is also bears the promise of new possibilities. The Wheel reminds us that nobody remains at the top forever, but also that not every situation is hopeless."
+        "description": "The Wheel of Fortune means that change is coming. One's destiny could turn out for the better or the worse, yet it is also bears the promise of new possibilities. The Wheel reminds us that nobody remains at the top forever, but also that not every situation is hopeless.",
+        "image": "images/cyberpunk2077/wheelOfFortune.png"
     },
     "12": {
         "name": "Justice",
-        "description": "Justice is the card of conflict resolution. It proclaims the need for order, to see through lies and deceit, and a return to the natural state of affairs. Justice implies a just sentence, but also due process."
+        "description": "Justice is the card of conflict resolution. It proclaims the need for order, to see through lies and deceit, and a return to the natural state of affairs. Justice implies a just sentence, but also due process.",
+        "image": "images/cyberpunk2077/justice.png"
     },
     "13": {
         "name": "The Hanged Man",
-        "description": "The Hanged Man is the card of sacrifice. It says that a price must be paid in order to achieve enlightenment. The Hanged Man's forsakenness opens the path for rebirth into a new life, though this path is wrought with pain suspended in time and ultimately ends in Death."
+        "description": "The Hanged Man is the card of sacrifice. It says that a price must be paid in order to achieve enlightenment. The Hanged Man's forsakenness opens the path for rebirth into a new life, though this path is wrought with pain suspended in time and ultimately ends in Death.",
+        "image": "images/cyberpunk2077/theHangedMan.png"
     },
     "14": {
         "name": "Death",
-        "description": "Death is the card of becoming. It signifies an imminent and difficult transition - the conclusion of one phase of life and the beginning of another. Inevitably, something gets lost during the transformation, but something else will rise to take its place."
+        "description": "Death is the card of becoming. It signifies an imminent and difficult transition - the conclusion of one phase of life and the beginning of another. Inevitably, something gets lost during the transformation, but something else will rise to take its place.",
+        "image": "images/cyberpunk2077/death.png"
     },
     "15": {
         "name": "Temperance",
-        "description": "Temperance is the card of balance. It may symbolize self-restraint or the gradual shift toward a more mature state of equilibrium. Temperance is associated with being in control of oneself as well as the desire to achieve inner peace."
+        "description": "Temperance is the card of balance. It may symbolize self-restraint or the gradual shift toward a more mature state of equilibrium. Temperance is associated with being in control of oneself as well as the desire to achieve inner peace.",
+        "image": "images/cyberpunk2077/temperance.png"
     },
     "16": {
         "name": "The Tower",
-        "description": "The Tower is an omen of radical change, chaos and destruction. The lightning striking The Tower signifies a return to the old order that lies buried under the ruins, and a new order that will rise from it. It is a symbol of tragedy, apocalypse and self-destruction."
+        "description": "The Tower is an omen of radical change, chaos and destruction. The lightning striking The Tower signifies a return to the old order that lies buried under the ruins, and a new order that will rise from it. It is a symbol of tragedy, apocalypse and self-destruction.",
+        "image": "images/cyberpunk2077/theTower.png"
     },
     "17": {
         "name": "The Moon",
-        "description": "The Moon reminds us that reality is not always what it seems at first glance. In a world of appearances and illusions, the best course is often charted by one's own intuition. The Moon is also the card of dreams, desires, and of course, sleep - Death's nightly ritual."
+        "description": "The Moon reminds us that reality is not always what it seems at first glance. In a world of appearances and illusions, the best course is often charted by one's own intuition. The Moon is also the card of dreams, desires, and of course, sleep - Death's nightly ritual.",
+        "image": "images/cyberpunk2077/theMoon.png"
     },
     "18": {
         "name": "The Sun",
-        "description": "The Sun symbolizes success. It is a card of freedom, renewal and a bright future that lies ahead. The Sun also represents truth, for its light will always pull back the curtain of shadow that hides the world's secrets. It also represents greatness and splendor."
+        "description": "The Sun symbolizes success. It is a card of freedom, renewal and a bright future that lies ahead. The Sun also represents truth, for its light will always pull back the curtain of shadow that hides the world's secrets. It also represents greatness and splendor.",
+        "image": "images/cyberpunk2077/theSun.png"
     },
     "19": {
         "name": "The World",
-        "description": "The World lies at the end of The Fool's long and winding journey. Wiser and more world-weary than he started out, the Fool faces a moment of reckoning. Some of us accept where out journey has led us to, while others embark on a new challenge. One thing in this world is certain -  you can't have it all."
+        "description": "The World lies at the end of The Fool's long and winding journey. Wiser and more world-weary than he started out, the Fool faces a moment of reckoning. Some of us accept where out journey has led us to, while others embark on a new challenge. One thing in this world is certain -  you can't have it all.",
+        "image": "images/cyberpunk2077/theWorld.png"
     }
 }
